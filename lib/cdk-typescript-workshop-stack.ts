3f50f503478362f3b00a6aaf0b4f8faa30004317
masatotezuka <tez.0731.mst@gmail.com>
2024-11-23 10:19:45 +0900

    add vpc

--- lib/cdk-typescript-workshop-stack.ts
+++ lib/cdk-typescript-workshop-stack.ts
@@ -1,8 +1,14 @@
 import { Stack, StackProps } from "aws-cdk-lib"
 import { Construct } from "constructs"
+import * as ec2 from "aws-cdk-lib/aws-ec2"
 
 export class CdkTypescriptWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
+
+    const vpc = new ec2.Vpc(this, "BlogVpc", {
+      vpcName: "BlogVpc",
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+    })
   }
 }
